b5a5f6dede472e750372a98ae4047ecb6d5635d3
Steven Paul Gray <aquasaurious@yahoo.com>
2017-03-03 18:22:59 -0700

    finished the bonus content, always remember  in controller

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,4 +1,8 @@
 angular.module('friendsList').controller('mainCtrl', function($scope) {
     $scope.friends = ["Hunnybunny", "Smuzz", "Frat", "Inka"];
 
+    $scope.addFriend = function() {
+        $scope.friends.push($scope.nufriend);
+        $scope.nufriend = '';
+    };
 });
